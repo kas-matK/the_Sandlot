217fba201d9250ce5d6b50f1ff00daf4ef6d6471
Kas <aformist@gmail.com>
2017-08-26 09:12:28 -0400

    Adding play temp and converting temp to perm

--- springSeason.js
+++ springSeason.js
@@ -10,11 +10,31 @@
 // }
 
 // RECURSION WAY
-function threeAndFive(num){
-    num--
-    if(num===0) return 0;
-    if((num%3===0)||(num%5===0)) return num + threeAndFive(num);
-    else return threeAndFive(num);
+// function threeAndFive(num){
+//     num--
+//     if(num===0) return 0;
+//     if((num%3===0)||(num%5===0)) return num + threeAndFive(num);
+//     else return threeAndFive(num);
+//
+// }
+// console.log(threeAndFive(1000));
 
+
+
+//PROBLEM 2
+
+function fibEven(num){
+    // if(num<3) return 0;
+    var runTot = 0;
+    var fibArr = [1,2];
+    var i = 1;
+    do {
+        if(fibArr[i]%2===0) runTot += fibArr[i];
+        i++;
+        fibArr[i] = (fibArr[i-1]+fibArr[i-2]);
+
+    } while(fibArr[i]<num);
+    return runTot;
 }
-console.log(threeAndFive(1000));
+
+console.log(fibEven(4000000));
--- winterSeason.py
+++ winterSeason.py
@@ -11,14 +11,45 @@
 
 # RECURSION WAY
 # DO NOT USE a myNum ABOVE 999
-def threeAndFive(myNum):
-    myNum -= 1
-    if myNum == 0:
-        return 0;
-    elif (myNum%3==0) or (myNum%5==0):
-        return myNum + threeAndFive(myNum)
-    else:
-        return threeAndFive(myNum)
+# def threeAndFive(myNum):
+#     myNum -= 1
+#     if myNum == 0:
+#         return 0;
+#     elif (myNum%3==0) or (myNum%5==0):
+#         return myNum + threeAndFive(myNum)
+#     else:
+#         return threeAndFive(myNum)
+#
+#
+# print(threeAndFive(999))
 
 
-print(threeAndFive(999))
+# Problem 2
+#LOOP WAY
+# def printFib(myNum):
+#     myArr = [0,1,1]
+#     if myNum < 0:
+#         print("You can't use a negative number!")
+#     elif myNum < 3:
+#         return myArr[0:myNum]
+#     else:
+#         for i in range(3,myNum+1):
+#             myArr.append(myArr[i-1] + myArr[i-2])
+#     return myArr[1:]
+#
+# print(printFib(10))
+
+def evenFibs(myNum):
+    myArr = [1,2]
+    runTot = 0
+    i = 1
+    while myArr[i] < myNum:
+        if myArr[i]%2 == 0:
+            runTot += myArr[i]
+        else:
+            runTot += 0
+        i += 1
+        myArr.append(myArr[i-1]+myArr[i-2])
+    return runTot
+
+print(evenFibs(4000000))
